078ec89b1519d32c51ca3da0f441aa97bcbdff34
AsHansen88 <ande970s@stud.kea.dk>
2023-11-27 16:40:35 +0100

    :)

--- app/screens/MapScreen.js
+++ app/screens/MapScreen.js
@@ -1,14 +1,24 @@
-import react from "react";
-import {Text, View} from 'react-native'
+import React from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+
 
 const MapScreen = () => {
     return(
-        <View style= {{flex: 1, justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "blue"}}>
-            <Text>Map Screen</Text>
-        </View>
+        <View style={styles.container}>
+      <MapView style={styles.map} />
+    </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    map: {
+      width: '100%',
+      height: '100%',
+    },
+  });
 
 export default MapScreen
